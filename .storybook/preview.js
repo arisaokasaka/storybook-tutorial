c5c12a4647b65083efab24df22cf9e31698b92aa
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2022-11-03 09:58:51 +0900

    add simple component - task

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import '../src/index.css'; // コンポーネント・storyに使用するスタイルをインポート
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
